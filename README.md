e49317e1123e0d03d81b7f86f45386ac91c8dbe8
cyril DIDIER <37507102+cyrildidier@users.noreply.github.com>
2018-03-30 17:43:37 +0200

    initial commit

--- README.md
+++ README.md
@@ -1,2 +1,6 @@
-# node-red-contrib-bmp180
-Node-Red node to get temperature, pressure and altitude from BMP180 sensor, connected by i2c. The node can run with metric units or imperial units.  Based on a self-modified dbridges's bmp085 node.js script for the core.
+@cyrildidier/node-red-contrib-bmp180
+=====================
+
+Node-Red node to get temperature, pressure and altitude from BMP180 sensor, connected by i2c.
+The node can run with metric units or imperial units. 
+Based on a self-modified dbridges's bmp085 node.js script for the core.
---
+++ bmp180.js
@@ -0,0 +1,28 @@
+var i2cbmp180 = require('./resources/i2cbmp180');
+
+module.exports = function(RED) {
+	
+	'------------------------ bmp180 ------------------------'
+	function bmp180(n) {
+		RED.nodes.createNode(this, n);
+		var node = this;		
+		var nodeAddress = n.address;
+		var nodeMode = n.mode;
+		var nodeUnits = n.units;
+		var nodeSeaLevel = n.sealevel;
+		node.on('input', function(msg) {
+			try {
+				var sensor = new i2cbmp180({address: nodeAddress,mode: nodeMode, units: nodeUnits, SeaLevel: nodeSeaLevel});
+				sensor.read(function (data){
+					var msg = { payload: data };
+					node.send(msg);
+				});
+			} catch (err) {
+				node.error(err);
+			}
+		})
+	}
+
+	'------------------------ RED node function register ------------------------'
+	RED.nodes.registerType('bmp180', bmp180);
+}
